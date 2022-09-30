33ea8bca9c960f7891b87a52d249b172e3916edc
Егор Архипцев <e.arkhiptsev@gmail.com>
2022-09-30 23:09:15 +0300

    last release

--- homework.py
+++ homework.py
@@ -59,7 +59,6 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        pass
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
